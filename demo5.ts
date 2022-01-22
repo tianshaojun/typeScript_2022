dcda227b717ecec03aeda84d47bee348c5dfb5c6
tianshaojun <you@example.com>
2022-01-22 20:20:07 +0800

    20220122study

---
+++ demo5.ts
@@ -0,0 +1,40 @@
+//数组
+const arr: (number | string)[] = [1, '2', 3];
+const stringArr: string[] = ['a','b','c'];
+const undefinedArr: undefined[] = [undefined];
+
+//type alias 类型别名
+type User = {name: string; age: number};
+
+const objectArr: User[] = [
+  {
+    name: 'dell',
+    age: 28
+  }
+];
+
+class Teacher {
+  name: string;
+  age: number
+}
+
+const objectArr2: Teacher[] = [
+  new Teacher(),
+  {
+    name: 'dell',
+    age: 28
+  }
+]
+
+//元组 tuple
+const teacherInfo: [string, string, number] = ['Dell', 'male', 18];
+//csv
+const teacherList: [string, string, number][] = [
+  ['dell','male',19],
+  ['sun','female',26],
+  ['jeny','female',38]
+]
+
+
+
+
