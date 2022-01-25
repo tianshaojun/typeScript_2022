739e5688f15dda8f85376d88da9e90bac31d1466
tianshaojun <you@example.com>
2022-01-25 18:56:56 +0800

    20220125晚上commit

--- typeScript/src/crowller.ts
+++ typeScript/src/crowller.ts
@@ -1,16 +1,40 @@
 // ts -> .d.ts 翻译文件 @types/superagent -> js
 import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Course {
+  title: string;
+  count: number;
+}
 
 class Crowller {
   private secret = 'secretKey';
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
 
-  private rawHtml = '';
+  getCourseInfo(html: string) {
+     const $ = cheerio.load(html);
+     const courseItems = $('.course-item');
+    //  console.log(courseItems);
+     const courseInfos: Course[] = [];
+     courseItems.map((index, element) => {
+        const descs = $(element).find('.course-desc');
+        const title = descs.eq(0).text();
+        const count = parseInt(descs.eq(1).text().split('：')[1], 10);
+        courseInfos.push({
+          title,
+          count
+        })
+     });
+     const result = {
+       time: (new Date()).getTime(),
+       data: courseInfos
+     }
+     console.log(result);
+  }
 
   async getRawHtml() {
     const result = await superagent.get(this.url);
-    // console.log(result.text);
-    this.rawHtml = result.text;
+    this.getCourseInfo(result.text);
   }
 
   constructor() {
