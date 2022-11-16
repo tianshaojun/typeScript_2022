b01dfae3324a4ce8bf64bbf59fbd186381ac1b2c
tianshaojun <you@example.com>
2022-11-16 20:28:36 +0800

    提交

---
+++ typeScript_Fe/src/crowller.ts
@@ -0,0 +1,38 @@
+// ts -> .d.ts 翻译文件 @types/superagent -> js
+import fs from 'fs'
+import path from 'path'
+import superagent from 'superagent'
+import DellAnalyzer from './dellAnalyzer'
+
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string
+}
+
+class Crowller {
+  private filePath = path.resolve(__dirname, '../data/course.json')
+
+  async getRawHtml() {
+    const result = await superagent.get(this.url)
+    return result.text
+  }
+
+  writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content)
+  }
+
+  async initSpiderProcess() {
+    const html = await this.getRawHtml()
+    const fileContent = this.analyzer.analyze(html, this.filePath)
+    this.writeFile(fileContent)
+  }
+
+  constructor(private url: string, private analyzer: Analyzer) {
+    this.initSpiderProcess()
+  }
+}
+
+const secret = 'secretKey'
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+
+const analyzer = new DellAnalyzer()
+new Crowller(url, analyzer)
