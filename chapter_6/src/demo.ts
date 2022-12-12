7f7c6fb5da1be5f38954f8f73602263ca42ea5f0
tianshaojun <you@example.com>
2022-12-12 15:14:47 +0800

    20221212

---
+++ chapter_6/src/demo.ts
@@ -0,0 +1,29 @@
+const userInfo: any = undefined;
+
+function catchError(msg: string) {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    const fn = descriptor.value;
+    descriptor.value = function () {
+      try {
+        fn();
+      } catch (e) {
+        console.log(msg);
+      }
+    };
+  };
+}
+
+class Test {
+  @catchError('userInfo.name 不存在')
+  getName() {
+    return userInfo.name;
+  }
+  @catchError('userInfo.age 不存在')
+  getAge() {
+    return userInfo.age;
+  }
+}
+
+const test = new Test();
+test.getName();
+test.getAge();
